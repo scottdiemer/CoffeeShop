bdf44512cd2818e4d992dfcc159b72ab76b06634
scottdiemer <sdiemer@cfl.rr.com>
2019-04-24 23:14:27 -0400

    pulled product infomation and mapped it to prroduct component

---
+++ src/components/Home/Product.js
@@ -0,0 +1,19 @@
+import React from "react"
+import Img from "gatsby-image"
+
+function Product({ product }) {
+  return (
+    <div className="col-10 col-sm-8 col-md-6 col-lg-4 mx-auto my-3">
+      <div className="card" style={{ minHeight: "100%" }}>
+        <Img className="card-img-top" fluid={product.image.fluid} />
+        <div className="card-body text-center">
+          <h6>{product.title}</h6>
+          <h6>${product.price.toFixed(2)}</h6>
+          <button className="btn btn-yellow mt-3 text-capitalize">add to cart</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Product
