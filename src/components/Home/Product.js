a7e7f37e6e3b676d961bd3b7dc25e17169ed2cbe
scottdiemer <sdiemer@cfl.rr.com>
2019-04-26 16:31:26 -0400

    added snipcart cart functionality

--- src/components/Home/Product.js
+++ src/components/Home/Product.js
@@ -9,7 +9,16 @@ function Product({ product }) {
         <div className="card-body text-center">
           <h6>{product.title}</h6>
           <h6>${product.price.toFixed(2)}</h6>
-          <button className="btn btn-yellow mt-3 text-capitalize">add to cart</button>
+          <button
+            className="btn btn-yellow mt-3 text-capitalize snipcart-add-item"
+            data-item-id={product.id}
+            data-item-name={product.title}
+            data-item-price={product.price}
+            data-item-image={product.image.fluid.src}
+            data-item-url="https://reg-joes.netlify.com/"
+          >
+            add to cart
+          </button>
         </div>
       </div>
     </div>
